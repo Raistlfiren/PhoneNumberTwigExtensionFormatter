b41a1dc18eb2e27cde49931edc1404bbd7df7c1c
Aaron Valandra <avaland@woopta.com>
2015-08-13 14:31:17 -0500

    Initial commit.

---
+++ Extension.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Bolt\Extension\Yourname\Extensionname;
+
+use Bolt\Application;
+use Bolt\BaseExtension;
+
+class Extension extends BaseExtension
+{
+
+    public function initialize() {
+        $this->addTwigFilter('phone', 'phoneNumberFilter');
+    }
+
+    public function phoneNumberFilter($phone)
+    {
+        $phone = preg_replace(
+            '~.*(\d{3})[^\d]{0,7}(\d{3})[^\d]{0,7}(\d{4}).*~', '($1) $2-$3',
+            $phone
+        );
+
+        return new \Twig_Markup($phone, 'UTF-8');
+    }
+
+    public function getName()
+    {
+        return "phone";
+    }
+
+}
+
+
+
+
+
+
---
+++ README.md
@@ -0,0 +1,7 @@
+Bolt Extension Phone Number Filter
+======================
+
+A twig filter that converts phone numbers from 1234567890 to (123) 456-7890.
+
+E.g.
+    {{ theme.phone|phone }}
